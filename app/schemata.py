bf7be0c341edc9dcee882bd6c07b917e9dd6e4a7
Fabian Sinz <sinz@bcm.edu>
2017-07-03 21:11:12 -0500

    add pipeline vis

--- app/schemata.py
+++ app/schemata.py
@@ -1,4 +1,7 @@
 import datajoint as dj
+
+
+vis = dj.create_virtual_module('vis','pipeline_vis')
 virus = dj.create_virtual_module('virus','common_virus')
 reso = dj.create_virtual_module('reso','pipeline_reso')
 experiment = dj.create_virtual_module('experiment','pipeline_experiment')
@@ -7,4 +10,4 @@ pupil = dj.create_virtual_module('pupil','pipeline_pupil')
 behavior = dj.create_virtual_module('behavior','pipeline_behavior')
 meso = dj.create_virtual_module('meso','pipeline_meso')
 experiment = dj.create_virtual_module('meso','pipeline_experiment')
-common_mice = dj.create_virtual_module('common_mice ','common_mice')
+mice = dj.create_virtual_module('mice ','common_mice')

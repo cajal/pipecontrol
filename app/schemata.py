43ce3deb6f9e8fe1ed4d08f381cdc03bd12a89b6
ecobost <emcobost@gmail.com>
2017-11-30 23:45:54 -0600

    add stack, behavior-> treadmill, drop vis

--- app/schemata.py
+++ app/schemata.py
@@ -2,11 +2,11 @@ import datajoint as dj
 
 reso = dj.create_virtual_module('reso','pipeline_reso')
 meso = dj.create_virtual_module('meso','pipeline_meso')
+stack = dj.create_virtual_module('stack','pipeline_stack')
 shared = dj.create_virtual_module('shared','pipeline_shared')
 experiment = dj.create_virtual_module('experiment','pipeline_experiment')
 pupil = dj.create_virtual_module('pupil','pipeline_eye')
-behavior = dj.create_virtual_module('behavior','pipeline_behavior')
+treadmill = dj.create_virtual_module('behavior','pipeline_treadmill')
 stimulus = dj.create_virtual_module('stimulus','pipeline_stimulus')
-vis = dj.create_virtual_module('vis','pipeline_vis')
 virus = dj.create_virtual_module('virus','common_virus')
 mice = dj.create_virtual_module('mice ','common_mice')
